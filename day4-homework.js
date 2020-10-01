dbf02424b633484066b4ea7f723bac2166a839f5
simonacossai <cossaisimona@gmail.com>
2020-10-01 14:45:52 +0200

    DONE- exercise number four

--- day4-homework.js
+++ day4-homework.js
@@ -45,6 +45,15 @@ console.log(crazyDiff(givenNumber, num));
 Write a function "boundary" which accept an integer N and returns true if N is within 20 and 100 (included) or equal to 400.
 */
 
+const boundary=(N)=>{
+    if(N>20 && N<100 || N===400){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(boundary(400));
+
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 5
